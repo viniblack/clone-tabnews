4029c4c0f513051c8356cf191376d389af2358a3
Vini Black <viniibl4ck@gmail.com>
2025-09-27 14:30:52 -0300

    add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(reuqest, response) {
+import database from "../../../../infra/database.js"
+
+async function status(reuqest, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows);
+
   response.status(200).json({ "chave": "Essa api é melhor que o milho verde" })
 }
 
