721abc9596a9973b93b6172cb12a5f1cdcdee0e4
Vini Black <viniibl4ck@gmail.com>
2025-09-29 22:15:37 -0300

    finish `/api/v1/status` endpoint

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,5 +1,14 @@
 test("GET to /api/v1/status shold return 200", async () => {
-  const response = await fetch("http://localhost:3001/api/v1/status")
+  const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200);
 
+  const responseBody = await response.json();
+
+  const parseUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 })
+
