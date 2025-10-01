531b5e5b0af6568324a93bcde7c44eee977cbbee
Vini Black <viniibl4ck@gmail.com>
2025-09-30 23:59:21 -0300

    fixed

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,7 @@
 import database from "infra/database.js"
-import { version } from "react";
 
 async function status(request, response) {
-  const updateAt = new Date().toISOString();
+  const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
@@ -18,7 +17,7 @@ async function status(request, response) {
   const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    update_at: updateAt,
+    updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,11 +1,12 @@
 test("GET to /api/v1/status shold return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
 
-  const parseUpdatedAt = new Date(responseBody.update_at).toISOString();
-  expect(responseBody.update_at).toEqual(parseUpdatedAt);
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();  
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
