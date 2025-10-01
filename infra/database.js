3fe6e6832af6434269703f0c415ea82e62ac92e4
Vini Black <viniibl4ck@gmail.com>
2025-10-01 00:00:40 -0300

    add more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,7 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === 'development' ? false : true,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -15,7 +16,8 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
-    console.error(err);
+    console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
@@ -23,4 +25,14 @@ async function query(queryObject) {
 
 export default {
   query: query
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
 }
