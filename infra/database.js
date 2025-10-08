81e5a41dadf2cbd599afb6788f5832cca79aa398
Vini Black <viniibl4ck@gmail.com>
2025-10-07 22:30:45 -0300

    adds  endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,20 @@
 import { Client } from 'pg';
 
 async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -11,20 +25,13 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  try {
-    await client.connect();
-    const result = await client.query(queryObject);
-    return result;
-  } catch (err) {
-    console.error(error);
-    throw error;
-  } finally {
-    await client.end();
-  }
+  await client.connect();
+  return client;
 }
 
 export default {
-  query: query
+  query,
+  getNewClient
 }
 
 function getSSLValues() {
@@ -34,5 +41,5 @@ function getSSLValues() {
     };
   }
 
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
---
+++ tests/api/v1/migrations/post.test.js
@@ -0,0 +1,43 @@
+import database from "infra/database.js";
+import fs from "fs";
+import path from "path";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;")
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: 'POST'
+  });
+  expect(response1.status).toBe(201);
+
+  const response1Body = await response1.json();
+
+  const folderPath = path.join(process.cwd(), 'infra/migrations');
+  const files = fs.readdirSync(folderPath).sort();
+  const sortedResponse1 = response1Body.sort((a, b) => a.name.localeCompare(b.name));
+
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: 'POST'
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Body = await response2.json();
+
+  const sortedResponse2 = response2Body.sort((a, b) => a.name.localeCompare(b.name));
+
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
+
+  for (let i = 0; i < Math.min(sortedResponse1.length, files.length); i++) {
+    const baseName = files[i].split(".")[0];
+    expect(sortedResponse1[i].name).toBe(baseName);
+    expect(response1Body.length).toBeGreaterThan(0);
+  }
+});
