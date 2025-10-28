e6de6efdb8b0fc70f51fb2e14c09172409d09f81
Vini Black <viniibl4ck@gmail.com>
2025-10-28 07:21:09 -0300

    feat: add initial version of `/status` page

---
+++ pages/status/index.js
@@ -0,0 +1,60 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdateAt />
+      <DatabaseStatus />
+    </>
+  );
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <div>Versão {data.dependencies.database.version}</div>
+        <div>
+          Conexões aberta: {data.dependencies.database.opened_connections}
+        </div>
+        <div>
+          Conexões máximas: {data.dependencies.database.max_connections}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Database</h2>
+      {databaseStatusInformation}
+    </>
+  );
+}
+
+function UpdateAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let updateAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    updateAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <div>Útilma atualização: {updateAtText}</div>;
+}
