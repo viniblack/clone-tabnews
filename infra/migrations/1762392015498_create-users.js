befe8916967f5469dbe44edc6bb784aed854010b
Vini Black <viniibl4ck@gmail.com>
2025-11-05 22:51:38 -0300

    feat: add migration to create `users` table

---
+++ infra/migrations/1762392015498_create-users.js
@@ -0,0 +1,45 @@
+
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()")
+    },
+
+    // Para refencia, limite de 39 caractres do GitHub.
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+
+    // Porque tamanho de 254? https://stackoverflow.com/a/1199238
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true
+    },
+
+    // Porque tamanho de 60: https://www.npmjs.com/package/bcrypt#hash-info
+    password: {
+      type: "varchar(60)",
+      notNull: true
+    },
+
+    // Porque salvar timestampt com time zone: https://justatheory.com/2012/04/postgres-use-timestamptz/
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())")
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())")
+    }
+  })
+};
+
+exports.down = false;
