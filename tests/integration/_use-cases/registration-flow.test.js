e85ea9559beec23d6c5b0746142805d58a65eb91
Vini Black <viniibl4ck@gmail.com>
2025-11-30 13:59:49 -0300

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/sessions`

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -81,7 +81,25 @@ describe("Use case: Registration Flow (all successful)", () => {
   });
 
   test("Login", async () => {
+    const createSessionsResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "registration.flow@black.com",
+          password: "RegistrationFlowPassword"
+        }),
+      },
+    );
 
+    expect(createSessionsResponse.status).toBe(201);
+
+    const createSessionsResponseBody = await createSessionsResponse.json();
+
+    expect(createSessionsResponseBody.user_id).toBe(createUserResponseBody.id);
   });
 
   test("Get user information", async () => {
