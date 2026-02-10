48059be164e5431263de1dd91bb26117f3d1d0e6
Vini Black <viniibl4ck@gmail.com>
2026-02-10 07:52:53 -0300

    feat: require `create:user` to access `/api/v1/users`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,7 +1,7 @@
-import orchestrator from "tests/orchestrator.js";
 import { version as uuidVersion } from "uuid";
-import user from "models/user";
-import password from "models/password";
+import orchestrator from "tests/orchestrator.js";
+import user from "models/user.js";
+import password from "models/password.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -18,8 +18,8 @@ describe("POST /api/v1/users", () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          username: "viniblack",
-          email: "vini@black.com",
+          username: "filipedeschamps",
+          email: "contato@curso.dev",
           password: "senha123",
         }),
       });
@@ -30,8 +30,8 @@ describe("POST /api/v1/users", () => {
 
       expect(responseBody).toEqual({
         id: responseBody.id,
-        username: "viniblack",
-        email: "vini@black.com",
+        username: "filipedeschamps",
+        email: "contato@curso.dev",
         features: ["read:activation_token"],
         password: responseBody.password,
         created_at: responseBody.created_at,
@@ -42,11 +42,12 @@ describe("POST /api/v1/users", () => {
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
 
-      const userInDatabase = await user.findOneByUsername("viniblack");
+      const userInDatabase = await user.findOneByUsername("filipedeschamps");
       const correctPasswordMatch = await password.compare(
         "senha123",
         userInDatabase.password,
       );
+
       const incorrectPasswordMatch = await password.compare(
         "SenhaErrada",
         userInDatabase.password,
@@ -64,7 +65,7 @@ describe("POST /api/v1/users", () => {
         },
         body: JSON.stringify({
           username: "emailduplicado1",
-          email: "duplicado@black.com",
+          email: "duplicado@curso.dev",
           password: "senha123",
         }),
       });
@@ -78,7 +79,7 @@ describe("POST /api/v1/users", () => {
         },
         body: JSON.stringify({
           username: "emailduplicado2",
-          email: "Duplicado@black.com",
+          email: "Duplicado@curso.dev",
           password: "senha123",
         }),
       });
@@ -103,7 +104,7 @@ describe("POST /api/v1/users", () => {
         },
         body: JSON.stringify({
           username: "usernameduplicado",
-          email: "usernameduplicado1@duplicado.com",
+          email: "usernameduplicado1@curso.dev",
           password: "senha123",
         }),
       });
@@ -116,8 +117,8 @@ describe("POST /api/v1/users", () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          username: "Usernameduplicado",
-          email: "usernameduplicado2@duplicado.com",
+          username: "UsernameDuplicado",
+          email: "usernameduplicado2@curso.dev",
           password: "senha123",
         }),
       });
@@ -131,6 +132,38 @@ describe("POST /api/v1/users", () => {
         message: "O username informado já está sendo utilizado.",
         action: "Utilize outro username para realizar esta operação.",
         status_code: 400,
+      });
+    });
+  });
+
+  describe("Default user", () => {
+    test("With unique and valid data", async () => {
+      const user1 = await orchestrator.createUser();
+      await orchestrator.activateUser(user1);
+      const user1SessionObject = await orchestrator.createSession(user1.id);
+
+      const user2Response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session_id=${user1SessionObject.token}`,
+        },
+        body: JSON.stringify({
+          username: "usuariologado",
+          email: "usuariologado@curso.dev",
+          password: "senha123",
+        }),
+      });
+
+      expect(user2Response.status).toBe(403);
+
+      const user2ResponseBody = await user2Response.json();
+
+      expect(user2ResponseBody).toEqual({
+        name: "ForbiddenError",
+        message: "Você não possui permissão para executar esta ação.",
+        action: 'Verifique se o seu usuário possui a feature "create:user"',
+        status_code: 403,
       });
     });
   });
